2ecf47631fb0ee8d17bf8c21e91400e3aacd0daa
edward sams <edwardsams@edwards-MacBook-Pro.local>
2023-05-01 17:15:45 -0400

    changes to UI and addition of pdf reader

--- src/app/common/common-routing.module.ts
+++ src/app/common/common-routing.module.ts
@@ -1,23 +1,23 @@
-import { inject, NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BlogLoginComponent } from './components/blog-login/blog-login.component';
-import { CreatePostComponent } from './components/create-post/create-post.component';
-import { PostComponent } from './components/post/post.component';
-import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
-import { BlogAdminGuard } from '../auth/guards/blog-admin.guard';
-import { PdfChatComponent } from './components/pdf-chat/pdf-chat.component';
+import { inject, NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { BlogLoginComponent } from './components/blog-login/blog-login.component'
+import { CreatePostComponent } from './components/create-post/create-post.component'
+import { PostComponent } from './components/post/post.component'
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component'
+import { BlogAdminGuard } from '../auth/guards/blog-admin.guard'
+import { PdfChatComponent } from './components/pdf-chat/pdf-chat.component'
 
 export const routeComponents = [
   PrivacyPolicyComponent,
   PostComponent,
   CreatePostComponent,
   BlogLoginComponent,
-  PdfChatComponent
-];
+  PdfChatComponent,
+]
 
 export const routes: Routes = [
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
-  { path: 'blog', component: PostComponent },
+  { path: 'news', component: PostComponent },
   {
     path: 'blog-admin',
     component: BlogLoginComponent,
@@ -31,7 +31,7 @@ export const routes: Routes = [
     path: 'pdf-chat',
     component: PdfChatComponent,
   },
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/common/components/pdf-chat/pdf-chat.component.ts
+++ src/app/common/components/pdf-chat/pdf-chat.component.ts
@@ -12,24 +12,49 @@ import { takeUntil } from 'rxjs'
 })
 export class PdfChatComponent extends SubResolver {
   public isDarkTheme$ = this._store.select(selectIsDarkTheme)
+  public qaHistory: { query: string; answer: string }[] = []
   public query = ''
   public answer = ''
+  public isLoadingAnswer = false
+  public pdfLoading = false
+  public pdfUploaded = false
 
   constructor(private _pdfService: PdfService, private _store: Store) {
     super()
   }
 
   public onUploadPdf(event: any) {
+    this.pdfLoading = true
     const pdfFile = event.target.files[0]
-    this._pdfService.embedAndUploadPdf(pdfFile).pipe(takeUntil(this.destroy$)).subscribe()
+    this._pdfService
+      .embedAndUploadPdf(pdfFile)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => {
+        if (res.message === 'PDF embedded and uploaded successfully.') {
+          this.pdfLoading = false
+          this.pdfUploaded = true
+        }
+      })
   }
 
   public onSubmitQuery() {
+    this.isLoadingAnswer = true
     this._pdfService
       .queryEmbeddedPdf(this.query)
       .pipe(takeUntil(this.destroy$))
       .subscribe((res) => {
         this.answer = res.answer
+        this.qaHistory.push({ query: this.query, answer: res.answer })
+        this.query = ''
+        this.isLoadingAnswer = false
       })
+  }
+
+  public resetComponent() {
+    this.query = ''
+    this.answer = ''
+    this.qaHistory = []
+    this.pdfLoading = false
+    this.pdfUploaded = false
   }
 }
